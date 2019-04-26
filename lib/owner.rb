ca31a3e2b9d29312acf21a9b29da821fe90a8d1e
Billy Joseph <ingbillyjoseph@gmail.com>
2019-04-26 12:41:28 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,19 @@
 class Owner
   # code goes here
+  class Owner
+  @@all= []
+  attr_accessor :name, :pets
+  attr_reader :species
+
+  # Instance Methods #
+
+  def initialize(species)
+    @species = species
+    @@all << self
+    @pets = {:fishes => [], :dogs => [], :cats => []}
+  end
+
+  def say_species
+    return "I am a #{@species}."
+  end
 end
